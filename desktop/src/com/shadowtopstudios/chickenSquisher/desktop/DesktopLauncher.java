c78e62a73110b2fd3385a5b26439f9f75b3793b1
ncooper4 <neal@shadowtopstudios.com>
2014-11-19 17:15:51 -0700

    initialized files
    created libgdx files, and eclipse files

---
+++ desktop/src/com/shadowtopstudios/chickenSquisher/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.shadowtopstudios.chickenSquisher.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.shadowtopstudios.chickenSquisher.chickenSquisher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new chickenSquisher(), config);
+	}
+}
