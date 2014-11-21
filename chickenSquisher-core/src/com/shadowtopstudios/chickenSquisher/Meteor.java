0ab705f21db55506dc462a0fe5514507d31ccbef
bdodroid <bdodroid@gmail.com>
2014-11-21 09:33:50 -0700

    added code to meteors
    added some code to the meteors and meteorcontainer classes

--- chickenSquisher-core/src/com/shadowtopstudios/chickenSquisher/Meteor.java
+++ chickenSquisher-core/src/com/shadowtopstudios/chickenSquisher/Meteor.java
@@ -1,5 +1,39 @@
 package com.shadowtopstudios.chickenSquisher;
 
-public class Meteor {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+public class Meteor {
+	
+	public int id;
+	public float shadowSpeed;
+	public float meteorSpeed;
+	public float sizeX;
+	public float sizeY;
+	public float spawnX;
+	public float spawnY;
+	private Texture mIMG;
+	private float meteorSize;
+	private float collisionSize;
+	
+	
+	public Meteor(int id, float sspeed, float mspeed, float startsize, float spawnx, float spawny, float meteorsize, float collisionsize){
+		shadowSpeed = sspeed;
+		meteorSpeed = mspeed;
+		sizeX = startsize;
+		sizeY = startsize;
+		spawnX = spawnx;
+		spawnY = spawny;
+		meteorSize = meteorsize;
+		collisionSize = collisionsize;
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void draw(SpriteBatch mBatch){
+		
+	}
+	
 }
--- chickenSquisher-core/src/com/shadowtopstudios/chickenSquisher/MeteorsContainer.java
+++ chickenSquisher-core/src/com/shadowtopstudios/chickenSquisher/MeteorsContainer.java
@@ -2,4 +2,18 @@ package com.shadowtopstudios.chickenSquisher;
 
 public class MeteorsContainer {
 
+	public Meteor[] meteorsArray;
+	
+	
+	public MeteorsContainer(World world, int arraySize){
+		
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void draw(){
+		
+	}
 }
